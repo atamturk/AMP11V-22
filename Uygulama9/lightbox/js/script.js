a82fa117d4de0e4e066aa7c4b3bdc8b1702f4c59
atamturk <fatihatamturk@gmail.com>
2022-12-28 08:49:16 +0300

    Sınava Hazırlık

--- Uygulama9/lightbox/js/script.js
+++ Uygulama9/lightbox/js/script.js
@@ -7,3 +7,35 @@ function closeModal() {
     document.getElementById("myModal").
     style.display = "none";
   }
+  var time=10;
+  //const  sayac = setInterval(mesajGoster,1000);
+  function mesajGoster() {  
+       
+       document.getElementById("goster").innerHTML += "<h1>" + time + "</h1>";
+       time--;
+    if (time == 0) {
+        durdur();
+    }
+  }
+
+  function durdur() {
+    clearInterval(sayac);
+  }
+
+  function kontrolEt() {
+    var ad,soyad,email,sifre,t_sifre;
+    var regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+.)+([.])+[a-zA-Z0-9.-]{2,4}$/;
+    sifre = document.getElementById("sifre").value;
+    t_sifre = document.getElementById("t_sifre").value;
+    ad = document.getElementById("ad").value;
+    email = document.getElementById("email").value;
+    if(ad == ""){
+     document.getElementById("hata").innerHTML += "Ad kısmını boş bırakmayınız." + "<br>"; 
+    } 
+    if(sifre != t_sifre){
+      document.getElementById("hata").innerHTML += "Şifreler aynı olmalı"; 
+    } 
+    if(regex.test(email)==false){
+      document.getElementById("hata").innerHTML += "Mailinizi kontrol ediniz"; 
+    }
+  }
